d3d381186cf86a0a0f9ec0bfe65d39e479a911e8
Mini Model <minimodel@metamodel.me>
2013-10-02 16:17:35 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels_demo_installer/languages/rm/default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/rm/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:01+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install']       = 'Installar la demo';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['reset']         = 'Redefinir la demo';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install']       = 'Installar la demo cun in click en il back-end.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']         = 'Davart questa demo';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about_tasks']   = 'Jau vegn ad exequir suandants pensums:';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['action']        = 'Operaziuns pussaivlas';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['installed']     = 'Gratulaziun, l\'installaziun è terminada cun success.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['missing_files'] = 'Tia installaziun n\'ademplescha betg suandantas premissas ch\'èn necessarias per exequir questa demo:';
+$GLOBALS['TL_LANG']['mm_demo']['see_live']             = 'Visitar nossa pagina per vesair en acziun.';
