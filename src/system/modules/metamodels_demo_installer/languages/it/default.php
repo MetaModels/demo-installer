8de8f8c1474f76ca8862c09abdde924fa60bb662
Mini Model <minimodel@metamodel.me>
2013-10-08 16:17:45 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels_demo_installer/languages/it/default.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/it/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:01+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']  = 'Informazioni su questa demo';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['action'] = 'Operazioni possibili';
+$GLOBALS['TL_LANG']['mm_demo']['see_live']      = 'Vai al sito per provare la demo in azione.';
