4f3f6e73c692e21906fb437f682d1ffea8459ded
Mini Model <minimodel@metamodel.me>
2013-10-15 00:18:33 +0200

    Updated translations from transifex.

--- src/system/modules/metamodels_demo_installer/languages/rm/default.php
+++ src/system/modules/metamodels_demo_installer/languages/rm/default.php
@@ -8,16 +8,29 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-10-02T00:12:01+02:00
+ * last-updated: 2013-10-02T16:17:35+02:00
  */
 
-
-$GLOBALS['TL_LANG']['mm_demo']['BTN']['install']       = 'Installar la demo';
-$GLOBALS['TL_LANG']['mm_demo']['BTN']['reset']         = 'Redefinir la demo';
-$GLOBALS['TL_LANG']['mm_demo']['DSC']['install']       = 'Installar la demo cun in click en il back-end.';
-$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']         = 'Davart questa demo';
-$GLOBALS['TL_LANG']['mm_demo']['HDL']['about_tasks']   = 'Jau vegn ad exequir suandants pensums:';
-$GLOBALS['TL_LANG']['mm_demo']['HDL']['action']        = 'Operaziuns pussaivlas';
-$GLOBALS['TL_LANG']['mm_demo']['HDL']['installed']     = 'Gratulaziun, l\'installaziun è terminada cun success.';
-$GLOBALS['TL_LANG']['mm_demo']['HDL']['missing_files'] = 'Tia installaziun n\'ademplescha betg suandantas premissas ch\'èn necessarias per exequir questa demo:';
-$GLOBALS['TL_LANG']['mm_demo']['see_live']             = 'Visitar nossa pagina per vesair en acziun.';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['do_not_install'] = 'Fermar il script da demo';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install']        = 'Installar la demo';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install_demo']   = 'Cumenzar il script da demo';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['reset']          = 'Redefinir la demo';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['do_not_install'] = 'Na, jau hai decidì da plitost betg installar la demo.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install']        = 'Installar la demo cun in click en il back-end.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install_demo']   = 'Gea, jau sun segir, installescha la demo e demolescha las datas ch\'jau hai enfin ussa.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['reset']          = 'Installescha danovamain la demo cun in click en il back-end. Quai è practic sche ti has giugà memia fitg e vuls anavos il stadi ch\'il team ha sviluppà.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']          = 'Davart questa demo';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about_tasks']    = 'Jau vegn ad exequir suandants pensums:';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['action']         = 'Operaziuns pussaivlas';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['installed']      = 'Gratulaziun, l\'installaziun è terminada cun success.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['missing_files']  = 'Tia installaziun n\'ademplescha betg suandantas premissas ch\'èn necessarias per exequir questa demo:';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['ensure']         = 'Quai è l\'ultim avertiment, clicca sin "Demo!" sutavart ed il program vegn a stizzar tut las infumraziuns en questa installaziun da Contao che n\'èn betg relevantas. Sche ti has insatge impurtant qua, sco la banca da datas, il cuntegn en tl_files u ils templates, saja pertschart che quai vegn tut STIZZA!';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['general']        = 'Saja conscient che exequir questa acziun vegn a STIZZAR TUT IL CUNTEGN sco themes, paginas, artitgels, elements da cuntegn etc da tia installaziun e CREAR BE ina installaziun da demo sco ch\'ella vegn purschida dal team da MetaModels. Ti vegns probablamain betg a vulair exequri quest sin ina pagina reala! Nus avain avertì tai!';
+$GLOBALS['TL_LANG']['mm_demo']['db_import']             = 'Importar il dump %s';
+$GLOBALS['TL_LANG']['mm_demo']['delete_rows']           = 'Stizzar %d colonna(s) da la tabella %s';
+$GLOBALS['TL_LANG']['mm_demo']['drop_table']            = 'Svidar la tabella %s';
+$GLOBALS['TL_LANG']['mm_demo']['install_file']          = 'Installar la datoteca %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_dir']            = 'Allontanar l\'ordinatur %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_file']           = 'Stizzar la datoteca %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_link']           = 'Allontanar il simlink %s';
+$GLOBALS['TL_LANG']['mm_demo']['see_live']              = 'Visitar nossa pagina per vesair en acziun.';
