5bfcb6123306f422eb8edb36846faa9f9d55b861
Mini Model <minimodel@metamodel.me>
2013-10-15 16:14:19 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels_demo_installer/languages/de/default.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2013-10-02T00:12:01+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['do_not_install'] = 'Demo-Script stoppen';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install']        = 'Demo installieren';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install_demo']   = 'Demo-Script starten';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['reset']          = 'Demo zurücksetzen';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['do_not_install'] = 'Nein, ich habe mich entschieden, die Demo nicht zu installieren.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install']        = 'Installieren Sie die Demo mit einem Klick aus dem Backend heraus.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install_demo']   = 'Ja, ich bin sicher, dass ich die Demo installieren möchte. Dabei werden alle meine bisherigen Daten gelöscht.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['reset']          = 'Setzen Sie die Demo mit einem Klick zurück. Diese Option ist nützlich, falls Sie viele Optionen getestet haben und zum ursprünglich vorgesehen Zustand zurückkehren möchten.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']          = 'Über diese Demo';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about_tasks']    = 'Ich werde die folgenden Aufgaben ausführen:';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['action']         = 'Mögliche Operationen';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['installed']      = 'Herzlichen Glückwunsch, die Installation wurde erfolgreich durchgeführt.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['missing_files']  = 'Ihrer Installation fehlen die folgenden Voraussetzungen, die nötig sind, um die Demo auszuführen:';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['ensure']         = 'DIES IST DIE LETZTE WARNUNG. Falls Sie jetzt "Demo!" anklicken werden alle bisher in dieser Contao-Installation vorhandenen Inhalte ersetzt, überschrieben oder gelöscht. Dies betrifft beispielsweise die Datenbank, Inhalte in files/tl_files-Ordner oder auch die Templates.
+ALLE DIESE INHALTE WERDEN ENTFERNT!';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['general']        = 'Bitte beachten Sie, dass jede Operation in diesem Bereich ALLE INHALTE - wie vordefinierte Themes, Artikel, Seiten, Inhaltselemente und mehr - aus Ihrer Installation LÖSCHT und Ihr System IN EIN REINES DEMO-SYSTEM UMWANDELT. Sie möchten diese Operation wahrscheinlich nicht auf eine produktive Webseite anwenden.';
+$GLOBALS['TL_LANG']['mm_demo']['db_import']             = 'Importiere Datenbank-Dump %s';
+$GLOBALS['TL_LANG']['mm_demo']['delete_rows']           = 'Lösche Spalte(n) %d aus Tabelle %s';
+$GLOBALS['TL_LANG']['mm_demo']['drop_table']            = 'Lösche Tabelle %s';
+$GLOBALS['TL_LANG']['mm_demo']['install_file']          = 'Installiere Datei %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_dir']            = 'Entferne Verzeichnis %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_file']           = 'Lösche Datei %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_link']           = 'Entferne Symlink %s';
+$GLOBALS['TL_LANG']['mm_demo']['see_live']              = 'Wechseln Sie zur Live-Seite, um die Demo anzusehen.';
