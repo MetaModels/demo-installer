fbae10193fcf20a5b218f6070f040c5c50b9deb8
Mini Model <minimodel@metamodel.me>
2015-09-18 10:34:28 +0200

    Updated translations from transifex.

---
+++ src/system/modules/metamodels_demo_installer/languages/ru/default.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/metamodels/language/ru/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-07-13T12:16:43+02:00
+ */
+
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['do_not_install'] = 'Остановить демо-сценарий';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install']        = 'Установить демо';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['install_demo']   = 'Запустить демо сценарий';
+$GLOBALS['TL_LANG']['mm_demo']['BTN']['reset']          = 'Сбросить демо';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['do_not_install'] = 'Нет, я решил не устанавливать демо.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install']        = 'Установить демо одним щелчком из панели управления.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['install_demo']   = 'Да я уверен, что двигаюсь дальше, устанавливаю демо и уничтожаю все данные в дальнейшем.';
+$GLOBALS['TL_LANG']['mm_demo']['DSC']['reset']          = 'Заново установить демо одним щелчком из панели управления. Это полезно, если вы экспериментировали слишком много и хотите получить обратно чистую демо-установку.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about']          = 'Об этом демо';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['about_tasks']    = 'Я выполню следующие задачи:';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['action']         = 'Возможные операции';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['installed']      = 'Поздравляем, установка прошла успешно.';
+$GLOBALS['TL_LANG']['mm_demo']['HDL']['missing_files']  = 'В этой установке отсутствуют следующие необходимые условия, которые необходимы для запуска этой демо:';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['ensure']         = 'Это последнее предупреждение, нажав кнопку "Demo!" ниже, вся информация, которая не является этим демо, в этой Contao установке, будет уничтожена. Так что если у вас есть что-то ценное, например, база данных, содержимое tl_files или шаблоны, все это вы ПОТЕРЯЕТЕ!';
+$GLOBALS['TL_LANG']['mm_demo']['WRN']['general']        = 'Пожалуйста, имейте в виду, что любые операции выполненные здесь, УДАЛЯТ ВЕСЬ КОНТЕНТ: темы, страницы, статьи, элементы контента и т.п. из вашей текущей установки и сбросят его в чистой демо-установке. Поэтому, НЕ ЗАПУСКАЙТЕ скрипт на работающем веб-сайте, мы предупреждали вас!';
+$GLOBALS['TL_LANG']['mm_demo']['db_import']             = 'Импорт дампа %s';
+$GLOBALS['TL_LANG']['mm_demo']['delete_rows']           = 'Удалить %d строк(у) из таблицы %s';
+$GLOBALS['TL_LANG']['mm_demo']['drop_table']            = 'Удалить таблицу %s';
+$GLOBALS['TL_LANG']['mm_demo']['install_file']          = 'Установить файл %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_dir']            = 'Переместить каталог %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_file']           = 'Удалить файл %s';
+$GLOBALS['TL_LANG']['mm_demo']['remove_link']           = 'Переместить символическую ссылку %s';
+$GLOBALS['TL_LANG']['mm_demo']['see_live']              = 'Перейдите на наш сайт, чтобы увидеть его в действии.';
+
